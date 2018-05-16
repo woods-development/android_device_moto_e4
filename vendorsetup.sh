1252c892f0e6ddebf2c114d809eb2487ab199355
Izaq Kull <izaqkull@Strec.GH>
2018-05-16 08:13:29 +0000

    woods: auto-copy 'SkUserConfig.h' & 'SkUserConfigManual.h' to Core

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,13 @@
 for var in eng user userdebug; do
   add_lunch_combo cosp_e4-$var
 done
+
+
+echo
+echo "Copying 'SkUserConfig.h' & 'SkUserConfigManual.h' to Core ..."
+cp external/skia/include/config/SkUserConfig.h external/skia/include/core
+echo ""
+cp external/skia/include/config/SkUserConfigManual.h external/skia/include/core
+echo
+echo "...!DONE!"
+echo
